04c569470264e2b70039a75f53bb629a6cf0910f
sai varun tangalla <tangallasaivarun6@gmail.com>
2019-10-15 22:02:51 -0700

    Using weather api

---
+++ src/components/Titles.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Titles = props => (
+    <div>
+    <h1>Weather Finder</h1>
+    <p>Find out temperature, conditions and more...</p>
+</div>
+);
+export default Titles;
